29cee3cf099f28919939657a1707cb4c535ab436
lenovo <461650578@qq.com>
2020-04-03 15:09:31 +0800

    task6-3

---
+++ task6-3/main.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+int main()
+{
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("1.jpg", 1);
+	if (srcMat.empty())
+
+		return -1;
+	//变换前三点坐标
+	const cv::Point2f src_pt[] = {
+		cv::Point2f(200,200),
+		cv::Point2f(250,200),
+		cv::Point2f(200,100)
+	};
+	//变换后三点坐标
+	const cv::Point2f dst_pt[] = {
+		cv::Point2f(300,100),
+		cv::Point2f(300,50),
+		cv::Point2f(200,100)
+	};
+	//计算仿射矩阵
+	cv::Mat affine_matrix = cv::getAffineTransform(src_pt, dst_pt);
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+
+	cv::imshow("src ", srcMat);
+	cv::imshow("dst ", dstMat);
+	cv::waitKey(0);
+
+}
